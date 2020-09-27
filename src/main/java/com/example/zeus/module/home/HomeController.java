4e228e51c7a3a6c3bfb00856656ec8596ca984e9
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-09-27 11:25:36 +0200

    home.html

--- src/main/java/com/example/zeus/module/home/HomeController.java
+++ src/main/java/com/example/zeus/module/home/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
@@ -14,6 +16,13 @@ public class HomeController {
                 17
         );
         model.addAttribute("person", person);
+
+        PersonDto person1= new PersonDto("Basia", "Kowalska", 18);
+        PersonDto person2= new PersonDto("Kasia", "Kowalska", 19);
+        PersonDto person3= new PersonDto("Asia", "Kowalska", 20);
+
+        List<PersonDto> list= List.of(person1,person2,person3);
+        model.addAttribute("list", list);
         return "home";
     }
 
